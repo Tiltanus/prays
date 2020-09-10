c548c314cceca4b4bae0b332047ee1cf2093ae64
Arseniy Lyubogashchinskiy <tarsenicum@mail.ru>
2020-09-10 19:57:41 +0800

    first release

--- prays/Form1.Designer.cs
+++ prays/Form1.Designer.cs
@@ -28,13 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 56);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- prays/Form1.cs
+++ prays/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace prays
 {
     public partial class Form1 : Form
     {
+        string[] fileText;
+
         public Form1()
         {
             InitializeComponent();
+            int x = Screen.PrimaryScreen.WorkingArea.Width;
+            ClientSize = new System.Drawing.Size(x, 50);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Top = Screen.PrimaryScreen.Bounds.Height - 150;
+            label1.Width = this.Width;
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                Application.Exit();
+            string filename = openFileDialog1.FileName;
+            String first_String, second_String;
+            fileText = System.IO.File.ReadAllLines(filename, Encoding.UTF8);
+            Thread prayThread = new Thread(ShowPray);
+            prayThread.Start();
+        }
+
+        private void ShowPray()
+        {
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                Invoke(new Action(() => label1.Text = fileText[i]));
+                Thread.Sleep(1000);
+            }
+            Application.Exit();
         }
     }
 }
